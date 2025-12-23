0636df026838aab115eee77e1f9006c8773a7d39
Jason Latz <159408658+Jason-Latz@users.noreply.github.com>
2025-12-23 08:40:19 -0600

    Mvp implementation of popup.js, adding event listener and blur function.

---
+++ popup.js
@@ -0,0 +1,36 @@
+
+
+
+const TARGET_SELECTOR = '[data-cy="secondary-coach-speech"]';
+const BLUR_PX = 8;
+
+document.getElementById("run").addEventListener("click", async() => {
+    
+    const tabs = await chrome.tabs.query({ active: true, currentWindow: true});
+    const tab = tabs[0];
+
+    if (!tab || !tab.id) return;
+
+
+    await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: blurElementBySelector,
+        args: [TARGET_SELECTOR, BLUR_PX],
+
+    });
+
+    window.close(); 
+
+});
+
+function blurElementBySelector(selector, BLUR_PX) {
+
+    const elmt = document.querySelector(selector);
+
+    if (!el) return;
+
+    el.style.filter = `blur(${blurPx}px)`;
+    el.style.webkitFilter = `blur(${blurPx}px)`;
+
+    el.style.display = el.style.display || "block";
+}
